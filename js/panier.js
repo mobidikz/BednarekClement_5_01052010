495ba3d6937636f966e6b51f4051cc5b1b688829
Clément <clement.bednarek@gmail.com>
2021-05-31 15:16:20 +0200

    :construction: Début de mise en place de bootstrap

--- js/panier.js
+++ js/panier.js
@@ -19,3 +19,33 @@ document.forms["shipping"].addEventListener("submit", function(e) {
         alert("Formulaire ok"); // Remplacer par une redirection vers la page confirmation de commande
     }
 })
+
+
+
+
+
+function meFonction (e){
+    let erreur;
+
+    let inputs = this;
+
+    for (var i = 0; i < inputs.length; i++) {
+        console.log(inputs[i]);
+        if (!inputs[i].value) {
+            erreur = "Veuillez renseigner tous les champs";
+        }
+    }
+
+    if (erreur) {
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+        return false;
+    } else {
+        alert("Formulaire ok"); // Remplacer par une redirection vers la page confirmation de commande
+    }
+
+}
+
+
+
+document.forms["shipping"].addEventListener("submit", meFonction(e) )
